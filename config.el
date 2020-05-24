9b49be04293ddc09419cf434bba8ae83fc1398f7
Jorge Contreras <jorge.contreras789@gmail.com>
2020-05-23 22:42:09 -0400

    added org-super-agenda package and configuration

--- config.el
+++ config.el
@@ -51,3 +51,22 @@
 ;;
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
+
+(def-package! org-super-agenda
+  :after org-agenda
+  :init
+  (setq org-super-agenda-groups '((:name "Today"
+                                  :time-grid t
+                                  :scheduled today)
+                           (:name "Due today"
+                                  :deadline today)
+                           (:name "Important"
+                                  :priority "A")
+                           (:name "Overdue"
+                                  :deadline past)
+                           (:name "Due soon"
+                                  :deadline future)
+                           (:name "Big Outcomes"
+                                  :tag "bo")))
+        :config
+        (org-super-agenda-mode))
